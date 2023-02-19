5b370b4df214294617e4b69057aa4525fac5f6da
pratxks <101081557+pratxks@users.noreply.github.com>
2023-02-19 12:17:58 +0530

    Added CountryService.kt function & RetrofitClientInstance.kt

--- app/src/main/java/app/plantdiary/individualassignment304832/Service/CountryService.kt
+++ app/src/main/java/app/plantdiary/individualassignment304832/Service/CountryService.kt
@@ -1,4 +1,9 @@
 package app.plantdiary.individualassignment304832.Service
 
+import app.plantdiary.individualassignment304832.dto.Country
+
 class CountryService {
+    fun fetchCountries(): List<Country>? {
+
+    }
 }
